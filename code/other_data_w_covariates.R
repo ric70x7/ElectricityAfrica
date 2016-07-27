9ef593151cf41e6149a4f133909c4efc5695ecbf
Ricardo Andrade <Ricardo.AndradePacheco@ucsf.edu>
2016-07-26 18:37:05 -0700

    removing unused lines

--- code/other_data_w_covariates.R
+++ code/other_data_w_covariates.R
@@ -43,37 +43,7 @@ for(yi in years){ #NOTE years < 2013
   pixels <- cellFromXY(afr, df.test[mask, c("lon", "lat")])
   df.test$pop[mask] <- afr[pixels]
 }
-#pop.list <- list(raster("data/Population/GPW3_2000.tif"),
-#                 raster("data/Population/GPW3_2005.tif"),
-#                 raster("data/Population/GPW3_2010.tif"),
-#                 raster("data/Population/GPW3_2015.tif"))
-#years <- sort(unique(df.test$year))
-#pop.years <- c(2000, 2005, 2010, 2015)
-#for(yi in years){
-#  mask <- df.test$year == yi
-#  pixels <- cellFromXY(pop.list[[1]], df.test[mask, c("lon", "lat")]) # Pixels are the same across pop files
-#  if(yi < 2005){
-#    fa <- 1
-#    fb <- 2
-#  }else{
-#    if(yi < 2010){
-#      fa <- 2
-#      fb <- 3
-#    }else{
-#      if(yi < 2015){
-#        fa <- 3
-#        fb <- 4
-#      }
-#    }
-#  }
-#  a <- (pop.years[fb] - yi) * .2
-#  b <- 1 - a
-#  df.test$pop[mask] <- a * getValues(pop.list[[fa]])[pixels] + b * getValues(pop.list[[fb]])[pixels]
-#}
-#pop2010.raw <- raster("data/Africa-POP-2010_africa2010ppp/africa2010ppp.tif")
-#pop2010 <- resample(pop2010.raw, afr)
-#pixels <- cellFromXY(pop2010, df.test[, c("lon", "lat")])
-#df.test$pop2010 <- getValues(pop2010)[pixels]
+
 
 # Standardize/transform values
 load("code_output/z_params.RData")
